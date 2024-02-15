d7009bfa98be79a9d785e6b695cb5aa4b790606a
uwie <louis.binwag@student.ateneo.edu>
2024-02-15 15:53:28 +0800

    Added recipe 1 & 2
    Test view for recipe 1 & 2.
    
    TODO:
    *Add Context
    *Add HTML for Recipe View.

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def recipe_list(request):
 
     return HttpResponse("this is just a test")
+
+def recipe_1(request):
+
+    return HttpResponse("this is just a test for recipe1")
