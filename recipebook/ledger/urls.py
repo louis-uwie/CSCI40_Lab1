3688dbde248e1eb6a0fe371b38c876698b552875
uwie <louis.binwag@student.ateneo.edu>
2024-02-19 17:16:15 +0800

    Update urls.py

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 
-    path('recipe/list/', views.recipe_list, name="recipe_list"),
+    path('recipes/list/', views.recipe_list, name="recipe_list"),
     path('recipe/1/', views.recipe_1, name="recipe_1"),
     path('recipe/2/', views.recipe_2, name="recipe_2")
 
