e1ac4be6b4480f57009fbc1bb471a15cec8cd617
uwie <louis.binwag@student.ateneo.edu>
2024-03-04 01:44:47 +0800

    Modified models.py & views.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}"
