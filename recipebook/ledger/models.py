64a09e71bb1256262f04c6ddbfaa8a483aeb85db
uwie <157711065+louis-uwie@users.noreply.github.com>
2024-03-04 13:30:34 +0800

    Update for readability

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-class Ingredient(models.Model):
+class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+    
+    
 
-
-
-class Recipe(models.Model):
+class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
