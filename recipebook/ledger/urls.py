ca03093f23b82fb50470c6aa85f2c676c9ceeca2
uwie <louis.binwag@student.ateneo.edu>
2024-03-04 12:20:32 +0800

    Adjusted templates, views, and urls accordingly

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     path('recipes/list/', views.recipe_list, name="recipe_list"),
+    path('recipes/<int:id>/', views.recipe_detail, name="recipe_detail"),
 
     ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Ingredient, RecipeIngredient, Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipeList.html', {'recipes':recipes})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    ingredients = recipe.recipeingredient_set.all()
+    context = {'recipe': recipe, 'ingredients': ingredients}
+    return render(request, 'recipe_1.html', context)
+
+
+
+
