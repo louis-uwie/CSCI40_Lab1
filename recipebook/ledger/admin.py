0b4af1d87565d87bdb1ef0fb08d43b8ebe1ab94d
uwie <157711065+louis-uwie@users.noreply.github.com>
2024-03-15 15:23:23 +0800

    update admin, models, and urls

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import Profile
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
+    verbose_name_plural = 'Profile'
 
-class UserAdmin(admin.BaseUserAdmin):
-    inlines = [ProfileInline,]
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
 
-    admin.site.unregister(User)
-    admin.site.register(User, UserAdmin)
+# Unregister the default User admin
+admin.site.unregister(User)
+
+# Register the custom User admin
+admin.site.register(User, CustomUserAdmin)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+def minimumBio(a):
+    if a < 255: raise ValidationError("Bio should be at least 255 characters.")
+
+'''
+Test Users:
+    username: Tyrone
+    password: usertest1
+'''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=50, blank=True)
+    user_bio = models.TextField(max_length=756, validators=[minimumBio])
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    author = models.ForeignKey('Profile', on_delete=models.CASCADE)
-    created_on = models.DateTimeField(default=timezone.now, editable=False)
-    updated_on = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None, null=True)
 
     def __str__(self):
         return self.name
@@ -32,9 +47,3 @@ class RecipeIngredient(models.Model):
     
     def get_absolute_url(self):
         return reverse('recipe_detail', args=[str(self.recipe.pk)])
-
-
-
-class Profile(models.Model):
-    name = models.TextField(max_length=50, blank=True)
-    user_bio = models.TextField(blank=True)
