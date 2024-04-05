9032b3918606bceb25b6809d71b19a2e774f1b2b
uwie <157711065+louis-uwie@users.noreply.github.com>
2024-04-05 12:49:13 +0800

    Updated Admin.py
    Included the following models for admin access:
    > Recipe, RecipeImages, Ingredients
    
    Tested adding Recipe to recipe_image/

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,25 +2,22 @@ from django.contrib import admin
 
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Profile, Recipe, RecipeImage
+from .models import Profile, Recipe, RecipeImage, Ingredient
 
 class ProfileInline(admin.StackedInline):
     model = Profile
     can_delete = False
     verbose_name_plural = 'Profile'
 
+
 class CustomUserAdmin(UserAdmin):
     inlines = (ProfileInline,)
 
 
-class Recipe():
-    model = Recipe
-    can_delete = True
-    verbose_name_plural = 'Recipes'
-
-class RecipeAdmin():
-    
-    pass
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+
+admin.site.register(Recipe)
+admin.site.register(RecipeImage)
+admin.site.register(Ingredient)
